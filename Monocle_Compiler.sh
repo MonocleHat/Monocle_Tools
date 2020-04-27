08a987fd340f3770095d7919039205b96b228045
Brandyn <57634447+MonocleHat@users.noreply.github.com>
2020-04-27 18:43:57 -0400

    Updated Compiler, functional build 1.0

--- Monocle_Compiler.sh
+++ Monocle_Compiler.sh
@@ -3,15 +3,16 @@ filearr=()
 outputnames=()
 if [ $# -eq 0 ];
 then
-	echo "USAGE: To compile scripts present in the booksrc directory and output them to compiledExamples directory "
+	echo "USAGE: for bulk compilation of scripts from one directory to be output to another "
 	echo "Example: "
 	echo "./compiler.sh -A ~/Documents/Code/UncompiledFiles filename.c filename.c filename.c "
 elif [ $1 == "-h" ];
 then
-	echo "DOCUMENTATION"
-	echo "This program can be used to compile multiple scripts present in a directory"
-	echo "Programs can be compiled based on if a user would like ASLR enabled or disabled. 
-This is done using the -En flag for normal compiling, and -Dis to disable ASLR checks for the program. Its worth noting that this just disables the 'canaries' and not actually disable ASLR system wide. please enable this on your system if you intend to use the Dis flag."
+	echo "[+] '-h' Display this list"
+	echo "[+] '-En' Regularly compile the files using gcc with stack protections/canaries"
+	echo "[+] '-Dis' Disable stack protections for gcc compiled programs, will not work if ASLR not disabled"
+	echo "[+] Disabling ASLR on a system: 'sudo bash -c 'echo 0 > /proc/sys/kernel/randomize_va_space'"
+	echo "[+] If it doesnt work for your system then there may be different instructions in place for your kernel. Read your manuals"
 elif [ $# -gt 0 ];
 then
 	if [ $1 == '-En' ];
